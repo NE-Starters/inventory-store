f5b90e9b46b3317fc8f8c79b6138a89850987290
jusish <jusish@jusish.mineduc.co>
2025-05-28 19:46:39 +0200

    [feat]: live reload

--- src/main/java/inventory/ne/manage/config/OpenApiConfig.java
+++ src/main/java/inventory/ne/manage/config/OpenApiConfig.java
@@ -11,30 +11,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Inventory Management API")
-                        .version("1.0.0")
-                        .description(
-                                "This API provides endpoints for managing inventory, including products, categories, and orders."))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Inventory Management API")
+                                                .version("1.0.0")
+                                                .description(
+                                                                "This API provides endpoints for managing inventory, including products, categories, and orders."))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new io.swagger.v3.oas.models.Components()
+                                                .addSecuritySchemes("bearerAuth",
+                                                                new SecurityScheme()
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")));
+        }
 
-    @Bean
-    public GroupedOpenApi authApi() {
-        return GroupedOpenApi.builder()
-                .group("auth")
-                .pathsToMatch("/api/auth/**")
-                .build();
-    }
+        @Bean
+        public GroupedOpenApi authApi() {
+                return GroupedOpenApi.builder()
+                                .group("auth")
+                                .pathsToMatch("/api/auth/**")
+                                .build();
+        }
 
- 
 }
